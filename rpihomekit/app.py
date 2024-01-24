b0d44eb138645b39896eaf9715596407db1fa414
giocaizzi <giocaizzi@gmail.com>
2024-01-25 00:35:26 +0100

    flat app layout and metadata

---
+++ rpihomekit/app.py
@@ -0,0 +1,25 @@
+from flask import Flask, jsonify
+from importlib import metadata
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return jsonify(
+        {
+            "Message": "Welcome to HomeKit",
+            "Version": metadata.metadata("rpihomekit")["version"],
+        }
+    )
+
+
+if __name__ == "__main__":
+    app.run(
+        # comment out debug=True when deploying to production
+        # debug=True,
+        # Setting host to "0.0.0.0" tells
+        # the os to listen on all public IPs.
+        host="0.0.0.0",
+        port=5000,
+    )
