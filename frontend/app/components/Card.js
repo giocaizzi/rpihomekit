9efc3833b0f3aa75f929d92a3921a8279ff8cc5b
giocaizzi <giocaizzi@gmail.com>
2024-01-28 21:12:59 +0100

    minimal

--- frontend/app/components/Card.js
+++ frontend/app/components/Card.js
@@ -1,6 +1,4 @@
 "use client"
-
-import { fetchData } from "../lib/getData.js"
 import './Card.css';
 
 
@@ -13,22 +11,4 @@ export function Card({ children }) {
 }
 
 export function WelcomeCard() {
-  // const [data, setData] = useState({ message: '', version: '' });
-
-  // useEffect(() => {
-  //   fetch('api/')
-  //     .then(response => response)
-  //     .then(data => setData(data))
-  //     .catch(error => console.error('Error:', error));
-  // }, []);
-
-  // console.log(data);
-  fetchData();
-  // return (
-  //   <Card>
-  //     {fetchData()}
-  //     {/* <h2>{data.message}</h2>
-  //     <div>Version: {data.version}</div> */}
-  //   </Card>
-  // );
 }
